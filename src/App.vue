36b92193d582dec73f1270811605479b54bd0270
akhmaos <akhmaos@mail.ru>
2021-02-11 13:42:19 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <input class="city" placeholder="Enter city" v-model="citySearch" type="text">
+    <h4>{{ countryCode }} {{ cityName }}</h4>
+    <table style="width:100%">
+      <tr style="text-align: left">
+        <th>Средняя температура</th>
+        <th>Скорость ветра</th>
+        <th>Описание</th>
+        <th>Давление</th>
+      </tr>
+      <tr v-if="$store.state.weather">
+        <td>{{ $store.state.weather.main.temp }}</td>
+        <td>{{ $store.state.weather.wind.speed }}</td>
+        <td>{{ $store.state.weather.weather[0].description }}</td>
+        <td>{{ $store.state.weather.main.pressure }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './styles/scss/main.scss'
+import {ref} from 'vue'
+import {mapGetters} from 'vuex'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup(){
+    const citySearch = ref('')
+
+    return {
+      citySearch
+    }
+  },
+  mounted() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        this.$store.dispatch('fetchWeatherByCoords', {lat: pos.coords.latitude, lon: pos.coords.longitude})
+      }, err => console.log(err));
+    }
+  },
+  watch: {
+    citySearch() {
+      if (this.timeout) clearTimeout(this.timeout)
+      this.timeout = setTimeout(() => {
+        this.$store.dispatch('fetchWeatherByCity', this.citySearch)
+      }, 500)
+    }
+  },
+  computed: {
+    ...mapGetters(['countryCode', 'cityName'])
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.city {
+  padding: 12px 24px;
+  width: 100%;
+  border: none;
+  font-size: 16px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,46 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import {createStore} from 'vuex'
+import axios from 'axios'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const appId = '886705b4c1182eb1c69f28eb8c520e20'
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?'
+
+const store = createStore({
+    state: {
+        weather: null,
+    },
+    actions: {
+        fetchWeatherByCity: async ({commit}, city) => {
+            try {
+                const {data} = await axios.get(`${baseUrl}appid=${appId}&q=${city}`)
+                commit('setWeather', data)
+            } catch (e) {
+                commit('setWeather', null)
+            }
+        },
+        fetchWeatherByCoords: async ({commit}, coords) => {
+            try {
+                const {data} = await axios.get(`${baseUrl}appid=${appId}&lat=${coords.lat}&lon=${coords.lon}`)
+                commit('setWeather', data)
+            } catch (e) {
+                commit('setWeather', null)
+            }
+        }
+    },
+    mutations: {
+        setWeather: (state, weather) => {
+            state.weather = weather
+        }
+    },
+    getters:{
+        cityName(s){
+            return s.weather ? s.weather.name : ''
+        },
+        countryCode(s){
+            return s.weather ? s.weather.sys.country : ''
+        }
+    }
+})
+
+createApp(App).use(store).mount('#app')
